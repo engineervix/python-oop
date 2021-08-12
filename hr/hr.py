78968d2578af5ee4ab872dd2d6f3fc851f0f53d5
Victor <victormiti@umusebo.com>
2021-08-12 21:59:44 +0200

    feat: Composition to Change Runtime Behavior

--- hr/hr.py
+++ hr/hr.py
@@ -29,6 +29,27 @@ class _PayrollSystem:
             print("")
 
 
+class LTDPolicy:
+    def __init__(self) -> None:
+        self._base_policy = None
+
+    def track_work(self, hours):
+        self._check_base_policy()
+        return self._base_policy.track_work(hours)
+
+    def calculate_payroll(self):
+        self._check_base_policy()
+        base_salary = self._base_policy.calculate_payroll()
+        return base_salary * 0.6
+
+    def apply_to_policy(self, base_policy):
+        self._base_policy = base_policy
+
+    def _check_base_policy(self):
+        if not self._base_policy:
+            raise RuntimeError("Base Policy Missing")
+
+
 class PayrollPolicy:
     def __init__(self) -> None:
         self.hours_worked = 0
--- run.py
+++ run.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
-import json
-
-from hr.employees import employee_database, Employee
-from hr.hr import calculate_payroll
+from hr.employees import employee_database
+from hr.hr import LTDPolicy, calculate_payroll
 from hr.productivity import track
-
-
-def print_dict(d):
-    print(json.dumps(d, indent=2))
-
 
 employees = employee_database.employees()
 
+sales_employee = employees[2]
+ltd_policy = LTDPolicy()
+sales_employee.apply_payroll_policy(ltd_policy)
+
 track(employees, 40)
 calculate_payroll(employees)
-
-temp_secretary = Employee(5)
-print("Temporary Secretary:")
-print_dict(temp_secretary.to_dict())
