196c796172e1ce6352cad84a1dc5e20c00398ab7
Victor <victormiti@umusebo.com>
2021-08-12 20:18:03 +0200

    refactor: Modifying Object Behavior With Composition

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from hr.employees import EmployeeDatabase
-from hr.hr import PayrollSystem
+from hr.hr import HourlyPolicy, PayrollSystem
 from hr.productivity import ProductivitySystem
 
 productivity_system = ProductivitySystem()
 payroll_system = PayrollSystem()
 employee_database = EmployeeDatabase()
 employees = employee_database.employees()
+
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
 
 productivity_system.track(employees, 40)
 payroll_system.calculate_payroll(employees)
